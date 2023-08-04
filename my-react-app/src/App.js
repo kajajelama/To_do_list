7c82a215da6c64a0fd92a3b9746db602783126d1
karolina.kocarova <karolina.kocarova@cgi.com>
2023-08-04 12:08:50 +0200

    my first react Hook

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,21 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <h1>Jednoduchá React aplikace</h1>
+      <input type="text" value={text} onChange={handleChange} />
+      <Footer />
     </div>
   );
 }
---
+++ my-react-app/src/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <footer>Footer komponenta</footer>;
+}
+
+export default Footer;
